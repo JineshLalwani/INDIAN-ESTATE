60195ee31326302cd31ece5262849ead26401bcf
Jinesh <jineshlalwani77@gmail.com>
2024-01-16 17:13:49 +0530

    styling header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,35 +3,33 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
-          </h1>
+    <header className='bg-slate-500 shadow-md' style={{ height: '60px' }}>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-2'> {/* Decreased padding */}
+        <Link to='/' className='flex items-center'>
+          <img src='Widelogo.png' alt="Logo" className="logo" style={{ width: '200px', height: '40px' }} />
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form className='bg-slate-300 p-1 rounded-lg flex items-center'> {/* Decreased padding */}
           <input
             type='text'
             placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            className='bg-transparent focus:outline-none px-2 py-1' // Decreased padding
+            style={{ width: '500px' }}
           />
           <FaSearch className='text-slate-600' />
         </form>
-        <ul className='flex gap-4'>
+        <ul className='flex gap-2'> {/* Decreased gap */}
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-slate-900 hover:underline'>
               Home
             </li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-slate-900 hover:underline'>
               About
             </li>
           </Link>
           <Link to='/sign-in'>
-            <li className=' text-slate-700 hover:underline'> Sign in</li>
+            <li className=' text-slate-900 hover:underline'> Sign in</li>
           </Link>
 
         </ul>
